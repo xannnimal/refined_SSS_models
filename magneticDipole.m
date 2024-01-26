0ba7a0ed655d950ca70a913f94679d3bb1b7b847
Xan McPherson <amcphers@uw.edu>
2024-01-26 10:03:43 -0800

    update magneticDipole

--- magneticDipole.m
+++ magneticDipole.m
@@ -1,25 +1,25 @@
 function magneticFlux = magneticDipole(chanpos,sensing_dir,grad_pos_ex,dip_pos, dip_mom, ch_types)
 % Iman and Xan 2024
-%Calculating the magnetic field from a dipole using Griffiths EM eq. 5.89
+% Calculating the magnetic field from a dipole using Griffiths EM eq. 5.89
 % dip_mom: magnetic moment 1x3
 % dip_pos: position vector, from the center of the magnetic dipole to where the magnetic field is measured
 % chanpos: channel position matrix nchanx3
 % sensing_dir: the orientation/measuring direction of the sensors
 % grad_pos_ex: for gradiometers, need EX positions, for OPM sensors this
-% part of the function is never used
+%                part of the function is never used
 ch_types=ch_types';
 mu0 = pi*4E-7; 
 rhat = dip_pos/norm(dip_pos);
-%make r vector from (x,y,z) pos and coil_hat for rhat of sensing direction
+%make r vector from (x,y,z) chanpos 
 for i=(1:size(chanpos,1))
     r(i)=sqrt(chanpos(i,1)^2+chanpos(i,2)^2+chanpos(i,3)^2);
-    %coil_hat(i)=sqrt(coilori(i,1)^2+coilori(i,2)^2+coilori(i,3)^2);
+    sensing_dir(i,:)=sensing_dir(i,:)/norm(sensing_dir(i,:));
 end
 %calculate B for each channel position
 for i=(1:size(chanpos,1))
     if ch_types(i)==1 %magnetometer calculations
         BField(i,:) = mu0/(4*pi)*(1/(r(i)^3))*(3*(dot(dip_mom,rhat)*rhat-dip_mom));
-        magneticFlux(i,:) =dot(sensing_dir(i,:),BField(i,:));
+        magneticFlux(i,:) =dot(BField(i,:),sensing_dir(i,:)); %no integral for point mags
     else %gradiometers, ch_types=0
         del_x = 0.0084*grad_pos_ex(i,:);
         plus_r = sqrt((chanpos(i,1)+del_x(1,1))^2+(chanpos(i,2)+del_x(1,2))^2+(chanpos(i,3)+del_x(1,3))^2);
@@ -29,9 +29,9 @@ for i=(1:size(chanpos,1))
         %B field 8.4 mm in the sensor's negative x-direction (EX)
         BField_min(i,:) = mu0/(4*pi)*(1/(min_r^3))*(3*(dot(dip_mom,rhat)*rhat-dip_mom));
         %flux = positive - negative, divide by the gradiometer baseline (16.8 mm)
-        flux_plus_dot=dot(sensing_dir(i,:),BField_plus(i,:));
-        flux_min_dot=dot(sensing_dir(i,:),BField_min(i,:));
-        magneticFlux(i,:)=(flux_plus_dot-flux_min_dot)/0.0168;
+        flux_plus=dot(BField_plus(i,:),sensing_dir(i,:));
+        flux_min=dot(BField_min(i,:),sensing_dir(i,:));
+        magneticFlux(i,:)=(flux_plus-flux_min)/0.0168;
     end
 end
 
