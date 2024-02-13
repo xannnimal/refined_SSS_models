36b6e7bb6557d1d6966c837e8c8a49a4acaab87a
imanf1 <imanf01@uw.edu>
2024-02-12 18:36:33 -0800

    updated simulated annealing

--- optimize_sensing_direction.m
+++ optimize_sensing_direction.m
@@ -1,4 +1,4 @@
-function conditionNumber = optimize_sensing_direction(angle,opm_matrix,R_hat,theta_hat,ch_types,Lin)
+function conditionNumber = optimize_sensing_direction(angle,opm_matrix,R_hat,phi_hat,theta_hat,ch_types,Lin)
 % input: 144 angles
 % fixed sensor positions calculate SSS matrix
 % find condition number
@@ -7,6 +7,8 @@ function conditionNumber = optimize_sensing_direction(angle,opm_matrix,R_hat,the
 
 %Sin_vsh_vv: r_sphere,R,EX,EY,EZ,ch_types,Lin
 %opt phi_hat
-[~,SNin]= Sin_vsh_vv([0,0,0]',opm_matrix',R_hat',theta_hat',angle',ch_types,Lin); %calculating SSS matrix
+% angle_mat = zeros(144,3);
+phi_hat(:,3)= angle;
+[~,SNin]= Sin_vsh_vv([0,0,0]',opm_matrix',R_hat',theta_hat',phi_hat',ch_types,Lin); %calculating SSS matrix
 conditionNumber = cond(SNin);
 end
