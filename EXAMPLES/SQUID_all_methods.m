a4f65f56cc8d39a4e68b06a8b821026237e4ace4
Xan McPherson <amcphers@uw.edu>
2024-04-12 09:08:43 -0700

    updated with correct dipole simulation
    Samu's implementation. All results are updated in paper on overleaf

--- EXAMPLES/SQUID_all_methods.m
+++ EXAMPLES/SQUID_all_methods.m
@@ -18,25 +18,23 @@ info = fiff_read_meas_info(filename);
 [raw] = fiff_setup_read_raw(filename);
 [data,times] = fiff_read_raw_segment(raw);
 
-% these do not work, error in FT_FILETYPE
-% hdr    = ft_read_header(name);
-% grad = ft_read_sens(filename, 'senstype', 'meg');
+%check with matricies from python- they match other than -1 bad channel
+% S_mne_python = load("C:/Users/xanmc/RESEARCH/compute_max_basis_SN.mat");
+% R_mne_python = load("C:/Users/xanmc/RESEARCH/R.mat");
+% EX_mne_python = load("C:/Users/xanmc/RESEARCH/EX.mat");
+% EY_mne_python = load("C:/Users/xanmc/RESEARCH/EY.mat");
+% EZ_mne_python = load("C:/Users/xanmc/RESEARCH/EZ.mat");
+% check_cond = cond(S_mne_python.S);
+% [U,sig,V]= svd(S_mne_python.S,'econ');
+% sig = diag(sig);
 
-S_mne_python = load("C:/Users/xanmc/RESEARCH/compute_max_basis_S.mat");
-R_mne_python = load("C:/Users/xanmc/RESEARCH/R.mat");
-EX_mne_python = load("C:/Users/xanmc/RESEARCH/EX.mat");
-EY_mne_python = load("C:/Users/xanmc/RESEARCH/EY.mat");
-EZ_mne_python = load("C:/Users/xanmc/RESEARCH/EZ.mat");
-
-check_cond = cond(S_mne_python.S);
-
-%% load coil orientation from fif file
-for i=1:306
-    R(:,i)=info.chs(i).loc(1:3,:);
-    EX(:,i)=info.chs(i).loc(4:6,:);
-    EY(:,i)=info.chs(i).loc(7:9,:);
-    EZ(:,i)=info.chs(i).loc(10:12,:);
-end
+% load coil orientation from fif file
+% for i=1:306
+%     R(:,i)=info.chs(i).loc(1:3,:);
+%     EX(:,i)=info.chs(i).loc(4:6,:);
+%     EY(:,i)=info.chs(i).loc(7:9,:);
+%     EZ(:,i)=info.chs(i).loc(10:12,:);
+% end
 
 for i=(1:size(EX,2))
     if mod(i,3)==0 %every third is a magnetometer
@@ -54,6 +52,7 @@ for i=(1:306)
         k=k;
     end
 end
+
 
 %% SSS expansions- multi origin interior
 %find semi major and minor
@@ -75,54 +74,67 @@ end
 [SNin_tot,~] = multiVSHin_singleVSHout(center1', center2',R,EX,EY,EZ,ch_types,Lin,Lout);
 %calculate single in/out
 [Sin,SNin] = Sin_vsh_vv([0,0,0]',R,EX,EY,EZ,ch_types,Lin);
-[Sin2,SNin2] = Sin_vsh_vv([0,0,0]',R_mne_python,EX_mne_python,EY_mne_python,EZ_mne_python,ch_types,Lin);
 [Sout,SNout] = Sout_vsh_vv([0,0,0]',R,EX,EY,EZ,ch_types,Lout);
 
-check = cond(Sin2);
-return
-%% generate time dependent dipoles
-dip_pos = [5,0,0]; %[Rx Ry Rz] (size Nx3)
-dip_mom = [0,0,1]; %(size 3xN
-% dip_pos_out = [0,0.25,0]; %[Rx Ry Rz] (size Nx3)
-% dip_mom_out = [1,1,1];%(size 3xN)
 
-% for field trip generated data
-freq=2;
-%dipole_data = single_dipole_sim(R',EZ',dip_pos,dip_mom,freq);
-% specify grad
-grad = [];
-grad.chanpos=R'*100; %convert to cm
-grad.coilpos = R'*100;
-grad.coilori= EZ'*100; 
-grad.senstype = 'meg';
-grad.tra= eye(size(R',1));
-grad.label = info.ch_names(1,1:306);
-% for i=1:size(R',1)
-%   grad.label{i} = sprintf('OPM%03d', i);
-% end
+%% generate dependent dipoles
+%current dipole using Samu's implementation of Sarvas
+k=1;
+for i=(1:306)
+    if ch_types(i)==1 %every third is a magnetometer
+        mags(k)=i;
+        k=k+1;
+    else
+        k=k;
+    end
+end
+rs=[0,0,0];
+q=[0,1,0]; %y direction
+r0=[0.05,0,0]; %5cm along x axis
+phi_0 = dipole_field_sarvas(rs',q',r0',R,EX,EY,EZ,mags)';
 
-% create a concentric 3-sphere volume conductor, the radius is the same as for the electrodes
-vol   = [];
-vol.r = 12 * [0.88 0.92 1.00]; % radii of spheres, the head radius is 12 cm
-vol.c = [1 1/80 1];            % conductivity
-vol.o = [0 0 0];               % center of sphere
-% vol.r = 10;
-% vol.o = [0 0 0];
-%The dipoles position and orientation have to be specified with
-cfg=[];
-headmodel = ft_prepare_headmodel(cfg, grad);
-cfg.sourcemodel.pos        = dip_pos; %[Rx Ry Rz] (size Nx3)
-cfg.sourcemodel.mom        = dip_mom; %[Qx Qy Qz] (size 3xN)
-cfg.sourcemodel.unit       = 'cm'; %string, can be 'mm', 'cm', 'm' (default is automatic)
-cfg.sourcemodel.frequency = freq;
-cfg.unit='cm';
-%cfg.headmodel     = vol; %structure with volume conduction model, see FT_PREPARE_HEADMODEL
-cfg.grad          = grad; %structure with gradiometer definition or filename, see FT_READ_SENS
-%dipole_data = ft_dipolesimulation(cfg);
-dipole_data = ft_prepare_leadfield(cfg);
-% phi_0= dipole_data.trial{1,1}(:,:);
-% times = dipole_data.time{1, 1};
-return
+%with fieldtrip
+% dip_pos = [5,0,0]; %[Rx Ry Rz] (size Nx3)
+% dip_mom = [0,0,1]; %(size 3xN
+% % dip_pos_out = [0,0.25,0]; %[Rx Ry Rz] (size Nx3)
+% % dip_mom_out = [1,1,1];%(size 3xN)
+% 
+% % for field trip generated data
+% freq=2;
+% %dipole_data = single_dipole_sim(R',EZ',dip_pos,dip_mom,freq);
+% % specify grad
+% grad = [];
+% grad.chanpos=R'*100; %convert to cm
+% grad.coilpos = R'*100;
+% grad.coilori= EZ'*100; 
+% grad.senstype = 'meg';
+% grad.tra= eye(size(R',1));
+% grad.label = info.ch_names(1,1:306);
+% % for i=1:size(R',1)
+% %   grad.label{i} = sprintf('OPM%03d', i);
+% % end
+% 
+% % create a concentric 3-sphere volume conductor, the radius is the same as for the electrodes
+% vol   = [];
+% vol.r = 12 * [0.88 0.92 1.00]; % radii of spheres, the head radius is 12 cm
+% vol.c = [1 1/80 1];            % conductivity
+% vol.o = [0 0 0];               % center of sphere
+% % vol.r = 10;
+% % vol.o = [0 0 0];
+% %The dipoles position and orientation have to be specified with
+% cfg=[];
+% headmodel = ft_prepare_headmodel(cfg, grad);
+% cfg.sourcemodel.pos        = dip_pos; %[Rx Ry Rz] (size Nx3)
+% cfg.sourcemodel.mom        = dip_mom; %[Qx Qy Qz] (size 3xN)
+% cfg.sourcemodel.unit       = 'cm'; %string, can be 'mm', 'cm', 'm' (default is automatic)
+% cfg.sourcemodel.frequency = freq;
+% cfg.unit='cm';
+% %cfg.headmodel     = vol; %structure with volume conduction model, see FT_PREPARE_HEADMODEL
+% cfg.grad          = grad; %structure with gradiometer definition or filename, see FT_READ_SENS
+% %dipole_data = ft_dipolesimulation(cfg);
+% dipole_data = ft_prepare_leadfield(cfg);
+% % phi_0= dipole_data.trial{1,1}(:,:);
+% % times = dipole_data.time{1, 1};
 
 %add time dependence to dipole moment
 % f_start = 100; % start frequency
@@ -169,24 +181,7 @@ return
 % view(135, 20);
 % hold off
 
-%using Samu's function
-% m=dip_mom';
-% r0=dip_pos';
-% NX=EX;
-% NY=EY;
-% NZ=EZ;
-% magtype='gegen';
-% k=1;
-% for i=(1:306)
-%     if ch_types(i)==1 %every third is a magnetometer
-%         mags(k)=i;
-%         k=k+1;
-%     else
-%         k=k;
-%     end
-% end
-% vuovec = mkentta(m,r0,R,NX,NY,NZ,mags,magtype);
-% phi_0=vuovec';
+
 %% reconstrct internal data
 %%check mags vs grads
 j=1;
@@ -249,29 +244,42 @@ mVSH_sVSH=[SNin_tot SNout];
 oid_oid=[SNin_spm,SNout_spm];
 oid_sVSH=[SNin_spm,SNout];
 
-check_data_vsh_vsh_mags = subspace(phi_mags, [SNin_mags SNout_mags])*180/pi;
-check_data_vsh_vsh_grads = subspace(phi_grads, [SNin_grads SNout_grads])*180/pi;
-for i=(1:306)
-    check_data_vsh_vsh_d(i) = subspace(phi_0(i,:), sVSH_sVSH)*180/pi;
-    check_data_mvsh_vsh_d(i) = subspace(phi_0(i,:), mVSH_sVSH)*180/pi;
-    check_data_oid_oid_d(i) = subspace(phi_0(i,:), oid_oid)*180/pi;
-    check_data_oid_vsh_d(i) = subspace(phi_0(i,:), oid_sVSH)*180/pi;
-end
-check_data_vsh_vsh_dmin = min(check_data_vsh_vsh_d);
-check_data_vsh_vsh_dmax = max(check_data_vsh_vsh_d);
-check_data_vsh_vsh_dav = mean(check_data_vsh_vsh_d);
+%check subspace angles- for one time point
+angle_sVSHin=subspace(phi_0(:,1),SNin)*180/pi;
+angle_sVSH_sVSH=subspace(phi_0(:,1),[SNin SNout])*180/pi;
+angle_sVSH_mags = subspace(phi_mags(:,1),SNin_mags)*180/pi;
+angle_sVSH_grads = subspace(phi_grads(:,1),SNin_grads)*180/pi;
 
-check_data_mvsh_vsh_dmin = min(check_data_mvsh_vsh_d);
-check_data_mvsh_vsh_dmax = max(check_data_mvsh_vsh_d);
-check_data_mvsh_vsh_dav = mean(check_data_mvsh_vsh_d);
+angle_mVSHin = subspace(phi_0(:,1),SNin_tot)*180/pi;
+angle_mVSH_sVSH = subspace(phi_0(:,1),mVSH_sVSH)*180/pi;
+angle_oidin = subspace(phi_0(:,1),SNin_spm)*180/pi;
+angle_oid_oid = subspace(phi_0(:,1),oid_oid)*180/pi;
+angle_oid_sVSH = subspace(phi_0(:,1),oid_sVSH)*180/pi;
 
-check_data_oid_oid_dmin = min(check_data_oid_oid_d);
-check_data_oid_oid_dmax = max(check_data_oid_oid_d);
-check_data_oid_oid_dav = mean(check_data_oid_oid_d);
-
-check_data_oid_vsh_dmin = min(check_data_oid_vsh_d);
-check_data_oid_vsh_dmax = max(check_data_oid_vsh_d);
-check_data_oid_vsh_dav = mean(check_data_oid_vsh_d);
+%check data for signals with time 
+% check_data_vsh_vsh_mags = subspace(phi_mags, [SNin_mags SNout_mags])*180/pi;
+% check_data_vsh_vsh_grads = subspace(phi_grads, [SNin_grads SNout_grads])*180/pi;
+% for i=(1:306)
+%     check_data_vsh_vsh_d(i) = subspace(phi_0(i,:), sVSH_sVSH)*180/pi;
+%     check_data_mvsh_vsh_d(i) = subspace(phi_0(i,:), mVSH_sVSH)*180/pi;
+%     check_data_oid_oid_d(i) = subspace(phi_0(i,:), oid_oid)*180/pi;
+%     check_data_oid_vsh_d(i) = subspace(phi_0(i,:), oid_sVSH)*180/pi;
+% end
+% check_data_vsh_vsh_dmin = min(check_data_vsh_vsh_d);
+% check_data_vsh_vsh_dmax = max(check_data_vsh_vsh_d);
+% check_data_vsh_vsh_dav = mean(check_data_vsh_vsh_d);
+% 
+% check_data_mvsh_vsh_dmin = min(check_data_mvsh_vsh_d);
+% check_data_mvsh_vsh_dmax = max(check_data_mvsh_vsh_d);
+% check_data_mvsh_vsh_dav = mean(check_data_mvsh_vsh_d);
+% 
+% check_data_oid_oid_dmin = min(check_data_oid_oid_d);
+% check_data_oid_oid_dmax = max(check_data_oid_oid_d);
+% check_data_oid_oid_dav = mean(check_data_oid_oid_d);
+% 
+% check_data_oid_vsh_dmin = min(check_data_oid_vsh_d);
+% check_data_oid_vsh_dmax = max(check_data_oid_vsh_d);
+% check_data_oid_vsh_dav = mean(check_data_oid_vsh_d);
 
 
 
@@ -281,28 +289,29 @@ check_data_oid_vsh_dav = mean(check_data_oid_vsh_d);
 % data_time=dipole_data.time{1,1};
 % data_chan_num=dipole_data.trial{1,1}(chan_num,:); 
 
-figure(2);
-hold on;
-plot(times,phi_0(1,:))
-%plot(times,phi_in(1,:))
-%plot(times,phi_out(1,:))
-%plot(times,phi_0(1,:))
-%plot(times,data_rec_vsh_mags(1,:))
-%plot(times,data_rec_vsh_grads(1,:))
-plot(times,data_rec_vsh(1,:))
-%plot(times,data_rec_multi_vsh(1,:))
-%plot(times,data_rec_sph_sph(1,:))
-%plot(times,data_rec_sph_vsh(1,:))
-title('SQUID, Currrent Dipole at 5cm x')
-xlabel('time')
-ylabel('T')
-%ylim([-8e-12 8e-12])
-%legend({'Dip In','VSH Mags','VSH Grads', 'VSH/VSH'},'location','northwest')
-%legend({'Raw Data','VSH/VSH','Multi/VSH','Spm/Spm','Spm/VSH'},'location','northwest')
-legend({'Raw Data','VSH/VSH'},'location','northwest')
-hold off
+% figure(2);
+% hold on;
+% plot(times,phi_0(1,:))
+% %plot(times,phi_in(1,:))
+% %plot(times,phi_out(1,:))
+% %plot(times,phi_0(1,:))
+% %plot(times,data_rec_vsh_mags(1,:))
+% %plot(times,data_rec_vsh_grads(1,:))
+% plot(times,data_rec_vsh(1,:))
+% %plot(times,data_rec_multi_vsh(1,:))
+% %plot(times,data_rec_sph_sph(1,:))
+% %plot(times,data_rec_sph_vsh(1,:))
+% title('SQUID, Currrent Dipole at 5cm x')
+% xlabel('time')
+% ylabel('T')
+% %ylim([-8e-12 8e-12])
+% %legend({'Dip In','VSH Mags','VSH Grads', 'VSH/VSH'},'location','northwest')
+% %legend({'Raw Data','VSH/VSH','Multi/VSH','Spm/Spm','Spm/VSH'},'location','northwest')
+% legend({'Raw Data','VSH/VSH'},'location','northwest')
+% hold off
 
 
+return
 
 %% quantify differences between SSS methods using subspace angles
 %calculations independent of the type of raw data
