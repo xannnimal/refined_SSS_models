205ba42c30f882ff4d416b33857d7ecc3665a42c
Xan McPherson <amcphers@uw.edu>
2024-04-18 17:20:40 -0700

    try to use a different headmodel for ft_prepareleadfield

--- SQUID_dipole_sim_check.m
+++ SQUID_dipole_sim_check.m
@@ -11,6 +11,48 @@ center2= [-0.00433911, 0.04081329, 0.05194245];
 center1 = center1 - [0,0,0.05];
 center2 = center2 - [0,0,0.05];
 
+
+%%%%%%%%%%%% example from fieldtrip
+%--------------------------------------------------------------------------------------
+% making a leadfield using the single-sphere headmodel that is
+% produced with CTF software
+%--------------------------------------------------------------------------------------
+
+% read header, which contains the gradiometer description
+hdr  = ft_read_header('Subject01.ds');
+grad = hdr.grad;
+
+% read headshape
+shape = ft_read_headshape('Subject01.shape');
+shape = rmfield(shape, 'fid'); %remove the fiducials->these are stored in MRI-voxel
+
+% read in the single sphere models produced with CTF software
+ctf_ss = ft_read_headmodel('Subject01.hdm');
+
+% plotting the head model together with the head shape
+ft_plot_sens(grad);
+ft_plot_headmodel(ctf_ss, 'facecolor', 'cortex');
+ft_plot_headshape(shape);
+
+% prepare the leadfield for the single sphere model
+cfg                  = [];
+cfg.grad             = grad;
+cfg.headmodel        = ctf_ss;
+cfg.resolution       = 1;
+cfg.unit             = 'cm';
+sourcemodel_ctf_ss   = ft_prepare_leadfield(cfg);
+
+% use the same geometry for the grid in what is to follow
+sourcemodel = removefields(sourcemodel_ctf_ss, {'leadfield', 'leadfielddimord', 'label'});
+
+
+return
+
+
+
+
+
+
 %% generate SQUID magnetometers
 coordsys = 'device'; 
 rawfile = "sample_audvis_raw.fif";
